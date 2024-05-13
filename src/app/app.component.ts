6429328ff5fbbbd10f00d221f84fdba10b05ff3d
salihcodev <ahmedsalihdev@gmail.com>
2024-05-13 14:18:29 +0300

    (initial): Upload the changes so far.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,74 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'psw-generator';
+  password = '';
+
+  pswConfigs = {
+    includeLetters: false,
+    includeNumbers: false,
+    includeSymbols: false,
+    length: 8,
+  };
+
+  handleToggle(type: any) {
+    switch (type) {
+      case `includeLetters`:
+        this.pswConfigs.includeLetters = !this.pswConfigs[`includeLetters`];
+        break;
+      case `includeNumbers`:
+        this.pswConfigs.includeNumbers = !this.pswConfigs[`includeNumbers`];
+        break;
+      case `includeSymbols`:
+        this.pswConfigs.includeSymbols = !this.pswConfigs[`includeSymbols`];
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  setLength(e: Event) {
+    const length = (event?.target as HTMLInputElement)?.value;
+
+    this.pswConfigs.length = Number(length);
+  }
+
+  onBtnClick() {
+    const configSets = {
+      L: 'abcdefghijklmnopqrstvuwxyz',
+      N: '0123456789',
+      S: '~!@#$%^&*()',
+    };
+
+    let wholePswSet: string = ``;
+    if (this.pswConfigs.includeLetters) {
+      wholePswSet += configSets.L;
+    }
+
+    if (this.pswConfigs.includeNumbers) {
+      wholePswSet += configSets.N;
+    }
+
+    if (this.pswConfigs.includeSymbols) {
+      wholePswSet += configSets.S;
+    }
+
+    if (wholePswSet.length !== 0) {
+      let newPsw: string = ``;
+      for (let i = 0; i < this.pswConfigs.length; i++) {
+        const randIdx: number = Math.floor(Math.random() * wholePswSet.length);
+
+        newPsw += wholePswSet[randIdx];
+      }
+
+      this.password = newPsw;
+    } else {
+      alert(
+        `Please choose at least one criteria i.e <letters>, <symbols>, <numbers>`
+      );
+    }
+  }
 }
