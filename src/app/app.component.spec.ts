a31de27c623604cf1b9ebd10fe50adedbcd19a69
salihcodev <ahmedsalihdev@gmail.com>
2024-05-30 19:02:43 +0300

    (finish): Wrap up the app.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'listing' title`, () => {
+  it(`should have the 'typing' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('listing');
+    expect(app.title).toEqual('typing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, listing');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, typing');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CardComponent } from './card/card.component';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CardComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
 export class AppComponent {
-  list = [
-    {
-      title: 'The #1',
-      desc: 'lorem test the test #1',
-      img: 'https://placehold.co/600x400',
-    },
-    {
-      title: 'The #2',
-      desc: 'lorem test the test #2',
-      img: 'https://placehold.co/600x400',
-    },
-    {
-      title: 'The #3',
-      desc: 'lorem test the test #3',
-      img: 'https://placehold.co/600x400',
-    },
-    {
-      title: 'The #4',
-      desc: 'lorem test the test #4',
-      img: 'https://placehold.co/600x400',
-    },
-    {
-      title: 'The #5',
-      desc: 'lorem test the test #5',
-      img: 'https://placehold.co/600x400',
-    },
-  ];
+  randomtext = faker.lorem.sentence();
+  entredText = ``;
+
+  onInput(value: Event) {
+    this.entredText = (value?.target as HTMLInputElement).value;
+  }
+
+  afterCompare(currLetter: string, randLetter: string) {
+    if (!randLetter) {
+      return `pending`;
+    }
+
+    return currLetter === randLetter ? `correct` : `wrong`;
+  }
 }
